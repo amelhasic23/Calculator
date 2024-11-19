615735b6663039977dc4eef13639e54bacbf6603
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-19 10:49:05 +0100

    Commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercises
+{
+    class AddTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = x + y;
+
+            Console.WriteLine(z);
+        }
+    }
+}
